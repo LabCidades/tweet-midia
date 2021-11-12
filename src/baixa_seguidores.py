848c809037a36bdde64218e11552bd0887a44ed0
RicardoRosal01 <crosalri@jci.com>
2021-11-12 20:18:42 -0300

    alterações followers

--- src/baixa_seguidores.py
+++ src/baixa_seguidores.py
@@ -18,16 +18,16 @@ api = tw.API(auth, wait_on_rate_limit=True)
 
 lista_perfis = ['@agmural','@perifasemove','@chavosodausp','@favelaempauta','@ccsp_oficial','@PRODUTORAABANCA','@luanapsol','@RACIONAISCN','@criolomc','@CulturaSP','@ducavendish','@MajiwkiJacques','@arthurmoledoval','@LuisAdorno','@augustosnunes','@jnascim','@J_LIVRES','@GuilhermeBoulos','@MidiaNINJA']
 
-lista_perfis2 = ['@luanapsol','@RACIONAISCN','@criolomc','@CulturaSP','@ducavendish','@MajiwkiJacques','@arthurmoledoval','@LuisAdorno','@augustosnunes','@jnascim','@J_LIVRES','@GuilhermeBoulos','@MidiaNINJA']
+lista_perfis2 = ['@agmural']
 
 # extração dos tweets e criação do dataframe
 followers = []
 
-for perfil in lista_perfis2:
-    for follower in tw.Cursor(api.get_followers, screen_name=perfil, tweet_mode="extended").items(1000):
+for perfil in lista_perfis:
+    for follower in tw.Cursor(api.get_followers, screen_name=perfil, tweet_mode="extended").items():
       followers.append([perfil,follower])
       #time.sleep(20)
     print('=== extraindo tweets do :',perfil)
     df = pd.json_normalize(followers)
     compression_opts = dict(method='zip',archive_name='dataset_seguidores_'+perfil+'.csv')
-    df.to_csv('dataset_seguidores_'+perfil+'.zip', index=False, compression=compression_opts)
+    df.to_csv('dataset_tweets'+perfil+'.zip', index=False, compression=compression_opts)
--- src/baixa_tweets.py
+++ src/baixa_tweets.py
@@ -19,20 +19,20 @@ api = tw.API(auth, wait_on_rate_limit=True)
 
 #parametros de busca
 lista_perfis = ['@agmural','@perifasemove','@chavosodausp','@favelaempauta','@ccsp_oficial','@PRODUTORAABANCA','@luanapsol','@RACIONAISCN','@criolomc','@CulturaSP','@ducavendish','@MajiwkiJacques','@arthurmoledoval','@LuisAdorno','@augustosnunes','@jnascim','@J_LIVRES','@GuilhermeBoulos','@MidiaNINJA']
-lista_perfis_2 = ['@GuilhermeBoulos','@MidiaNINJA']
+lista_perfis_2 = ['@agmural','@perifasemove','@chavosodausp','@favelaempauta','@ccsp_oficial','@PRODUTORAABANCA','@luanapsol','@RACIONAISCN','@criolomc','@CulturaSP','@ducavendish','@MajiwkiJacques','@arthurmoledoval','@LuisAdorno','@augustosnunes','@jnascim','@J_LIVRES','@GuilhermeBoulos','@MidiaNINJA']
 data_desde = "2020-02-26"
     
 # extração dos tweets e criação do dataframe
 
 for perfil in lista_perfis_2:
   tweets = []
-  for status in tw.Cursor(api.user_timeline, screen_name=perfil, tweet_mode="extended").items(1000):
+  for status in tw.Cursor(api.user_timeline, screen_name=perfil, tweet_mode="extended",count=200).items(10000):
       tweets.append(status._json)
   print('=== extraindo tweets do :',perfil)
   df = pd.json_normalize(tweets)
   compression_opts = dict(method='zip',archive_name='dataset_tweets_'+perfil+'.csv')
-  df.to_csv('dataset_tweets'+perfil+'.zip', index=False, compression=compression_opts)
-  time.sleep(900)
+  df.to_csv('dataset_tweets_3_'+perfil+'.zip', index=False, compression=compression_opts)
+  time.sleep(300)
 
 """ perfil = '@favelaempauta'
 tweets = []
